43fa8a71afc17444cb2677c19cc6590fbc576229
rbryan95 <berd1495@gmail.com>
2023-12-12 00:57:32 -0600

    Category Service and Main Menu addition

---
+++ CategoryService.cs
@@ -0,0 +1,101 @@
+using Microsoft.Extensions.Logging;
+using NWConsole.Model;
+using NLog;
+using Microsoft.EntityFrameworkCore;
+
+
+public class CategoryService
+{
+    private readonly NWContext _db;
+    private readonly NLog.ILogger _logger;
+
+    public CategoryService(NWContext db, NLog.ILogger logger)
+    {
+        _db = db;
+        _logger = logger;
+        
+    }
+
+    // Add a new category
+    public void AddCategory(string categoryName, string description)
+    {
+        var category = new Category { CategoryName = categoryName, Description = description };
+        _db.Categories.Add(category);
+        _db.SaveChanges();
+    }
+
+    // Edit a specified category
+    public void EditCategory(int categoryId, string newCategoryName, string newDescription)
+    {
+        var category = _db.Categories.Find(categoryId);
+        if (category != null)
+        {
+            category.CategoryName = newCategoryName;
+            category.Description = newDescription;
+            _db.SaveChanges();
+        }
+    }
+
+    // Display all categories
+    public void DisplayAllCategories()
+    {
+        var categories = _db.Categories.ToList();
+        foreach (var category in categories)
+        {
+            Console.WriteLine($"Category Name: {category.CategoryName}, Description: {category.Description}");
+        }
+    }
+
+    // Display all categories and their related active product data
+    public void DisplayAllCategoriesWithActiveProducts()
+    {
+        var categories = _db.Categories.Include(c => c.Products).ToList();
+        foreach (var category in categories)
+        {
+            Console.WriteLine($"Category Name: {category.CategoryName}");
+            var activeProducts = category.Products.Where(p => !p.Discontinued).ToList();
+            foreach (var product in activeProducts)
+            {
+                Console.WriteLine($"Product Name: {product.ProductName}");
+            }
+        }
+    }
+
+    // Display a specific category and its related active product data
+    public void DisplayCategoryWithActiveProductsById(int categoryId)
+    {
+        var category = _db.Categories.Include(c => c.Products)
+                                    .SingleOrDefault(c => c.CategoryId == categoryId && c.Products.Any(p => !p.Discontinued));
+
+        if (category != null)
+        {
+            Console.WriteLine($"Category: {category.CategoryName}");
+            foreach (var product in category.Products.Where(p => !p.Discontinued))
+            {
+                Console.WriteLine($"Product: {product.ProductName}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"No category found with ID: {categoryId}");
+        }
+    }
+    public void DisplayCategoryWithActiveProductsByName(string categoryName)
+    {
+        var category = _db.Categories.Include(c => c.Products)
+                                    .SingleOrDefault(c => c.CategoryName == categoryName && c.Products.Any(p => !p.Discontinued));
+
+        if (category != null)
+        {
+            Console.WriteLine($"Category: {category.CategoryName}");
+            foreach (var product in category.Products.Where(p => !p.Discontinued))
+            {
+                Console.WriteLine($"Product: {product.ProductName}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"No category found with name: {categoryName}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,102 +16,64 @@ try
 {
     var db = new NWContext();
     var productService = new ProductService(db, logger);
+    var categoryService = new CategoryService(db, logger);
     string choice;
     do
     {
         Console.WriteLine("1) Display Categories");
         Console.WriteLine("2) Add Category");
-        Console.WriteLine("3) Display Category and related products");
-        Console.WriteLine("4) Display all Categories and their related products");
-        Console.WriteLine("5) Add Product");
-        Console.WriteLine("6) Edit Product");
-        Console.WriteLine("7) Display Products");
-        Console.WriteLine("8) Display a specific Product");
+        Console.WriteLine("3) Edit a specific category");
+        Console.WriteLine("4) Display all Categories with active products");
+        Console.WriteLine("5) Display specific Category with active products");
+        Console.WriteLine("6) Add Product");
+        Console.WriteLine("7) Edit Product");
+        Console.WriteLine("8) Display Products");
+        Console.WriteLine("9) Display a specific Product");
         Console.WriteLine("\"q\" to quit");
         choice = Console.ReadLine();
         Console.Clear();
         logger.Info($"Option {choice} selected");
         if (choice == "1")
         {
-            var query = db.Categories.OrderBy(p => p.CategoryName);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{query.Count()} records returned");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            foreach (var item in query)
-            {
-                Console.WriteLine($"{item.CategoryName} - {item.Description}");
-            }
-            Console.ForegroundColor = ConsoleColor.White;
+            categoryService.DisplayAllCategories();
         }
         else if (choice == "2")
         {
-            Category category = new Category();
             Console.WriteLine("Enter Category Name:");
-            category.CategoryName = Console.ReadLine();
+            string categoryName = Console.ReadLine();
             Console.WriteLine("Enter the Category Description:");
-            category.Description = Console.ReadLine();
-            ValidationContext context = new ValidationContext(category, null, null);
-            List<ValidationResult> results = new List<ValidationResult>();
-
-            var isValid = Validator.TryValidateObject(category, context, results, true);
-            if (isValid)
-            {
-                // check for unique name
-                if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
-                {
-                    // generate validation error
-                    isValid = false;
-                    results.Add(new ValidationResult("Name exists", new string[] { "CategoryName" }));
-                }
-                else
-                {
-                    logger.Info("Validation passed");
-                    // TODO: save category to db
-                }
-            }
-            if (!isValid)
-            {
-                foreach (var result in results)
-                {
-                    logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
-                }
-            }
+            string description = Console.ReadLine();
+            categoryService.AddCategory(categoryName, description);
         }
         else if (choice == "3")
         {
-            var query = db.Categories.OrderBy(p => p.CategoryId);
-
-            Console.WriteLine("Select the category whose products you want to display:");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            foreach (var item in query)
-            {
-                Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
-            }
-            Console.ForegroundColor = ConsoleColor.White;
-            int id = int.Parse(Console.ReadLine());
-            Console.Clear();
-            logger.Info($"CategoryId {id} selected");
-            Category category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id);
-            Console.WriteLine($"{category.CategoryName} - {category.Description}");
-            foreach (Product p in category.Products)
-            {
-                Console.WriteLine($"\t{p.ProductName}");
-            }
+            Console.WriteLine("Enter Category ID:");
+            int categoryId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter new Category Name:");
+            string newCategoryName = Console.ReadLine();
+            Console.WriteLine("Enter new Description:");
+            string newDescription = Console.ReadLine();
+            categoryService.EditCategory(categoryId, newCategoryName, newDescription);
         }
         else if (choice == "4")
         {
-            var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
-            foreach (var item in query)
-            {
-                Console.WriteLine($"{item.CategoryName}");
-                foreach (Product p in item.Products)
-                {
-                    Console.WriteLine($"\t{p.ProductName}");
-                }
-            }
+            categoryService.DisplayAllCategoriesWithActiveProducts();
         }
         else if (choice == "5")
+        {
+            Console.WriteLine("Enter Category ID or Name:");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int categoryId))
+            {
+                categoryService.DisplayCategoryWithActiveProductsById(categoryId);
+            }
+            else
+            {
+                string categoryName = input;
+                categoryService.DisplayCategoryWithActiveProductsByName(categoryName);
+            }
+        }
+        else if (choice == "6")
         {
             Console.WriteLine("Enter Product Name:");
             string productName = Console.ReadLine();
@@ -119,7 +81,7 @@ try
             bool isDiscontinued = bool.Parse(Console.ReadLine());
             productService.AddProduct(productName, isDiscontinued);
         }
-        else if (choice == "6")
+        else if (choice == "7")
         {
             Console.WriteLine("Enter Product Id:");
             int productId = int.Parse(Console.ReadLine());
@@ -129,13 +91,13 @@ try
             bool isDiscontinued = bool.Parse(Console.ReadLine());
             productService.EditProduct(productId, productName, isDiscontinued);
         }
-        else if (choice == "7")
+        else if (choice == "8")
         {
             Console.WriteLine("Enter filter (all, discontinued, active):");
             string filter = Console.ReadLine();
             productService.DisplayProducts(filter);
         }
-        else if (choice == "8")
+        else if (choice == "9")
         {
             Console.WriteLine("Enter 1 to search by name, 2 to search by ID:");
             string searchChoice = Console.ReadLine();
